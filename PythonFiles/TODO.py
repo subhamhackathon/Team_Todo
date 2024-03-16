25f5435b6bbb4f83ec2816be8f58ed8414d97017
AbhaJaiswal <35805827+AbhaJaiswal@users.noreply.github.com>
2024-03-17 01:13:29 +0530

    Update TODO.py
    updated latest changes. 
    primary and secondary values automated via full pdf parsing

--- PythonFiles/TODO.py
+++ PythonFiles/TODO.py
@@ -5,9 +5,17 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 import requests
 import io
+import re
   
 app = Flask(__name__)  
 CORS(app, support_credentials=True)
+
+client = AzureOpenAI(
+                  azure_endpoint = "https://team-todo.openai.azure.com/openai/deployments/test-aj/chat/completions?api-version=2024-02-15-preview", 
+                  api_key="e347916c17ca472cb05c1469a1fc5836",  
+                  api_version="2024-02-15-preview"
+                )
+
 
 def getAnswersFromPDF(url, entity): 
 
@@ -24,160 +32,127 @@ def getAnswersFromPDF(url, entity):
                 # creating a pdf reader object 
                 reader = PdfReader(pdf_bytes) 
 
-                # printing number of pages in pdf file 
-                print(len(reader.pages)) 
+                
 
-                # getting a specific page from the pdf file 
-                page = reader.pages[1] 
+                expr = re.compile("[0-5]?\\. (.*?) (.*)")
 
-                # extracting text from page 
-                text = page.extract_text() 
-                #print(text) 
+                primary={}
+                secondary={}
+
+                for page_num in range(len(reader.pages)):  
+                                  # getting a specific page from the pdf file 
+                  page = reader.pages[page_num] 
+
+                  # extracting text from page 
+                  text = page.extract_text() 
+                  #print(text) 
+
+                  if len(text.split()) < 20:
+                    continue
+
+                  message_text = [
+                  {"role":"system","content":"You are an AI assistant that will understand the text provided by the user and give response only in the format Yes/No(Reason)"},
+                  {"role":"user","content":text},
+                  {"role":"user","content":"read the above text and give a yes or no answer for below questions - \
+                      1. Is there a traget for Net Zero emissions mentioned in the text?  give response only in the format Yes/No(Reason) \
+                      2. Is there a target for Diversity, Equity and Inclusion mentioned in the text? give response only in the format Yes/No(Reason)? \
+                      3. Is there an adopted goal for reducing emission intensity mentioned in the text? give response only in the format Yes/No(Reason)? \
+                      4. Is there a Renewable Electricity Target mentioned in the text? give response only in the format Yes/No(Reason)? \
+                      5. Is there a Circularity Stratergy & targets mentioned in the text? give response only in the format Yes/No(Reason)? \
+                      "
+                      }
+                  ]
+
+
+                  completion = client.chat.completions.create(
+                    model="test-aj", # model = "deployment_name"
+                    messages = message_text,
+                    temperature=0.4,
+                    max_tokens=800,
+                    top_p=0.95,
+                    frequency_penalty=0,
+                    presence_penalty=0,
+                    stop=None
+                  )
+
+                  print(page_num)
+
+                  print()
+                  ans = completion.choices[0].message.content
+
+                  print(ans)
+
+                  lines = ans.splitlines()
+
+             
+                  i=1
+                  for line in lines:
+                    result = expr.search(line)
+                    present=result.group(1)
+                    reason=result.group(2)
+                    if present == "Yes":
+                      primary[str(i)]=present
+                      if str(i) in secondary:
+                        secondary[str(i)]=secondary[str(i)] + ". " + reason
+                      else:
+                        secondary[str(i)]=reason
+                    i=i+1 
 
 
 
-
-                client = AzureOpenAI(
-                  azure_endpoint = "https://team-todo.openai.azure.com/openai/deployments/test-aj/chat/completions?api-version=2024-02-15-preview", 
-                  api_key="e347916c17ca472cb05c1469a1fc5836",  
-                  api_version="2024-02-15-preview"
-                )
-
-
-                message_text = [{"role":"system","content":"You are an AI assistant that helps people find information."},{"role":"user","content":text},{"role":"user","content":"what is the adopted goal for reducing emission intensity? Answer only with number and timeframe."}]
-
-                print("Question : what is the adopted goal for reducing emission intensity? Answer only with number.")
-
-                completion = client.chat.completions.create(
-                  model="test-aj", # model = "deployment_name"
-                  messages = message_text,
-                  temperature=0.7,
-                  max_tokens=800,
-                  top_p=0.95,
-                  frequency_penalty=0,
-                  presence_penalty=0,
-                  stop=None
-                )
-
-                print()
-                print(completion)
-
-                emissionReductionQ = {
-                    "question": "what is the interim emission reduction target",
+                print(primary)
+                print(secondary)
+                
+                netZeroTargetQ = {
+                    "question": "what is net zero target",
                     "esgType": "Environment",
-                    "esgIndicators": "InterimEmissionsReductionTarget",
-                    "primaryDetails": completion.choices[0].message.content,
-                    "secondaryDetails": "",
-                    "citationDetails": "string",
+                    "esgIndicators": "NetZeroTarget",
+                    "primaryDetails": primary["1"] if "1" in primary else "No",
+                    "secondaryDetails": getSecondaryAnswer("1"," Net Zero emission ",secondary),
+                    "citationDetails": "",
                     "pageNumber": 1
                 }
 
-
-                message_text = [{"role":"system","content":"You are an AI assistant that helps people find information."},{"role":"user","content":text},{"role":"user","content":"Is there a Diversity, Equity and Inclusion target? Answer should be 'yes' or 'no' or unclear' only."}]
-
-                print()
-                print("Is there a Diversity, Equity and Inclusion target? Answer in yes or no only.")
-                completion = client.chat.completions.create(
-                  model="test-aj", # model = "deployment_name"
-                  messages = message_text,
-                  temperature=0.7,
-                  max_tokens=800,
-                  top_p=0.95,
-                  frequency_penalty=0,
-                  presence_penalty=0,
-                  stop=None
-                )
-
-                print(completion.choices[0].message.content)
 
                 diversityQ = {
                     "question": "what is the Diversity, Equity and Inclusion target",
                     "esgType": "Social",
                     "esgIndicators": "DE&ITarget",
-                    "primaryDetails": completion.choices[0].message.content,
-                    "secondaryDetails": "",
+                    "primaryDetails": primary["2"] if "2" in primary else "No",
+                    "secondaryDetails": getSecondaryAnswer("2"," Diversity, Equity and Inclusion ", secondary),
                     "citationDetails": "",
                     "pageNumber": 1
                 }
 
-                message_text = [{"role":"system","content":"You are an AI assistant that helps people find information."},{"role":"user","content":text},{"role":"user","content":"Is there a net zero target? Answer should be 'yes' or 'no' or unclear' only."}]
 
-                print()
-                print("Is there a net zero target? Answer in yes or no only.")
-                completion = client.chat.completions.create(
-                  model="test-aj", # model = "deployment_name"
-                  messages = message_text,
-                  temperature=0.7,
-                  max_tokens=800,
-                  top_p=0.95,
-                  frequency_penalty=0,
-                  presence_penalty=0,
-                  stop=None
-                )
-
-                print(completion.choices[0].message.content)
-
-                netZeroTargetQ = {
-                    "question": "what is net zero target",
+                emissionReductionQ = {
+                    "question": "what is the interim emission reduction target",
                     "esgType": "Environment",
-                    "esgIndicators": "NetZeroTarget",
-                    "primaryDetails": completion.choices[0].message.content,
-                    "secondaryDetails": "",
-                    "citationDetails": "",
+                    "esgIndicators": "InterimEmissionsReductionTarget",
+                    "primaryDetails": primary["3"] if "3" in primary else "No",
+                    "secondaryDetails": getSecondaryAnswer("3"," emission reduction " ,secondary),
+                    "citationDetails": "string",
                     "pageNumber": 1
                 }
 
-
-                message_text = [{"role":"system","content":"You are an AI assistant that helps people find information."},{"role":"user","content":text},{"role":"user","content":"Is there a Renewable Electricity Target? Answer should be 'yes' or 'no' or unclear' only."}]
-
-                print()
-                print("Is there a Renewable Electricity Target? Answer in yes or no only.")
-                completion = client.chat.completions.create(
-                  model="test-aj", # model = "deployment_name"
-                  messages = message_text,
-                  temperature=0.7,
-                  max_tokens=800,
-                  top_p=0.95,
-                  frequency_penalty=0,
-                  presence_penalty=0,
-                  stop=None
-                )
-
-                print(completion.choices[0].message.content)
 
                 renewableElectricityQ = {
                     "question": "what is the Renewable Electricity Target",
                     "esgType": "Environment",
                     "esgIndicators": "RenewableElectricityTarget",
-                    "primaryDetails": completion.choices[0].message.content,
-                    "secondaryDetails": "",
+                    "primaryDetails": primary["4"] if "4" in primary else "No",
+                    "secondaryDetails": getSecondaryAnswer("4"," Renewable Electricity " ,secondary),
                     "citationDetails": "",
                     "pageNumber": 1
                 }
 
-                message_text = [{"role":"system","content":"You are an AI assistant that helps people find information."},{"role":"user","content":text},{"role":"user","content":"Is there a Circularity Stratergy & targets? Answer should be 'yes' or 'no' or unclear' only."}]
-
-                print()
-                print("Is there a Circularity Stratergy & targets? Answer in yes or no only.")
-                completion = client.chat.completions.create(
-                  model="test-aj", # model = "deployment_name"
-                  messages = message_text,
-                  temperature=0.7,
-                  max_tokens=800,
-                  top_p=0.95,
-                  frequency_penalty=0,
-                  presence_penalty=0,
-                  stop=None
-                )
-
-                print(completion.choices[0].message.content)
 
                 circularityStratergyQ = {
                     "question": "what is the Circularity Stratergy & targets",
                     "esgType": "Environment",
-                    "esgIndicators": "RenewableElectricityTarget",
-                    "primaryDetails": completion.choices[0].message.content,
-                    "secondaryDetails": "",
+                    "esgIndicators": "CircularityStratergy",
+                    "primaryDetails": primary["5"] if "5" in primary else "No",
+                    "secondaryDetails": getSecondaryAnswer("5"," Circularity Stratergy ",secondary),
                     "citationDetails": "",
                     "pageNumber": 1
                 }
@@ -195,9 +170,32 @@ def getAnswersFromPDF(url, entity):
                 return jsonify(esgResponse)
 
 
+def getSecondaryAnswer(key, targetName , secondary):
+  secondary_ans = ""
+  if key in secondary:
+    message_text = [
+                      {"role":"system","content":"You are an AI assistant that will understand the text provided by the user and give response"},
+                      {"role":"user","content":"is there a specific target for" + targetName +  "mentioned in the below text??\
+                      Response should only have the target number and year. No extra words or reasoning . \
+                      Return one word 'No' if no target is mentioned. \
+                      \
+                      " + secondary[key]}
+                    ]
+    completion = client.chat.completions.create(
+      model="test-aj", # model = "deployment_name"
+      messages = message_text,
+      temperature=0.7,
+      max_tokens=800,
+      top_p=0.95,
+      frequency_penalty=0,
+      presence_penalty=0,
+      stop=None
+    )
+    secondary_ans = "" if completion.choices[0].message.content == "No." else completion.choices[0].message.content
+  return secondary_ans
+
 
 @app.route('/ESGReport')  
-@cross_origin(supports_credentials=True) 
 def hello():  
   url = request.args.get('pdf', 'spx.pdf')  
   entity = request.args.get('entity', 'spx') 
